669df8577ef6283f4e3a28ede6788ddb5fe5edab
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-10-14 18:44:16 -0500

    commit tree f45da4bb0593f1db50d4177cf555cca87bb54a10

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+const headQ = 42;
+
+function distanceFromHqInBlocks(pickup){
+  if (headQ > pickup){
+    return headQ - pickup
+  } else {
+    return pickup - headQ
+  }
+//  or simplified: Math.abs(headQ - pickup)
+}  
+
+
+function distanceFromHqInFeet(someValue){
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feet = blocks * 264;
+    return feet;
+}       
+
+
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs((end - start) * 264);
+}
+
+
+
+function calculatesFarePrice(start, end){
+    let traveled = distanceTravelledInFeet(start, end);
+    let billable = Math.max(0, traveled - 400);
+    
+    if (traveled <= 400){
+      console.log('gives customers a free sample');
+      return 0;
+    } else if (traveled > 400 && traveled <= 2000) {
+      return billable * 0.02;
+    } else if (traveled > 2000 && traveled <= 2500){
+      return 25;
+    } else if (traveled > 2500) {
+      return 'cannot travel that far'
+    }
+}
+    
+
+    
+    
+    
+    
